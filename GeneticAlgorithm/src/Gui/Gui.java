8e09467a46004b91c4077a697b932cf7f4cd4e23
atle95 <atle95@gmail.com>
2016-03-23 20:26:26 -0600

    quick update

--- GeneticAlgorithm/src/Gui/Gui.java
+++ GeneticAlgorithm/src/Gui/Gui.java
@@ -1,5 +1,20 @@
 package Gui;
 
-public class Gui {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Gui extends Application
+{
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+  
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
 
 }
