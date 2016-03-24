0017edc0a61f3fd376380353d98e055ba68262d0
atle95 <atle95@gmail.com>
2016-03-23 20:47:19 -0600

    Added new Gui, will continue to make it better

--- GeneticAlgorithm/src/Gui/Gui.java
+++ GeneticAlgorithm/src/Gui/Gui.java
@@ -1,14 +1,22 @@
 package Gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Gui extends Application
 {
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
-    // TODO Auto-generated method stub
+  public void start(Stage primaryStage) throws Exception 
+  {
+    Parent page = (Parent) FXMLLoader.load(Gui.class.getResource("Gui.fxml"));
+    Scene scene = new Scene(page);
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("Genetic Algorithm by Atle and Chris");
+    primaryStage.show();
     
   }
   
